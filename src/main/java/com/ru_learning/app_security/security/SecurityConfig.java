9b403deb378372a3982ad32cbcec8404dd961fad
OscarRu <oscar.ruizhu@gmail.com>
2025-07-08 16:11:21 -0600

    Add CSRF cookie filter and enhance security configurations
    Introduce `CsrfCookieFilter` to set CSRF tokens in response headers. Update `SecurityConfig` to configure CSRF token handling, enable CORS with permissive settings, and attach the CSRF cookie filter to the security chain for improved protection.

--- src/main/java/com/ru_learning/app_security/security/SecurityConfig.java
+++ src/main/java/com/ru_learning/app_security/security/SecurityConfig.java
@@ -14,20 +14,35 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.util.InMemoryResource;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        var requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         http.authorizeHttpRequests(auth ->
                 auth.requestMatchers("/loans", "/balance", "/accounts", "/cards")
                         .authenticated()
                         .anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler)
+                .ignoringRequestMatchers("/welcome", "/about_us")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
         return http.build();
     }
 
@@ -67,4 +82,24 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(); to_be_encoded
     }
     */
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSources(){
+        var config = new CorsConfiguration();
+
+        //config.setAllowedOrigins(List.of("http//localhost:4200/","http//my-app.com" ));
+        config.setAllowedOrigins(List.of("*"));
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("*"));
+
+        //config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        config.setAllowedHeaders(List.of("*"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
